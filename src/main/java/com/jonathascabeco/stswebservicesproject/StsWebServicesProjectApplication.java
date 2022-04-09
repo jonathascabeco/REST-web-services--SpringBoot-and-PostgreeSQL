d92fb07bf8ff8381bbcf01619d4d6f46bac7ef0e
jonathascabeco <jonathascabeco@gmail.com>
2022-04-09 14:22:24 -0300

    Project created

---
+++ src/main/java/com/jonathascabeco/stswebservicesproject/StsWebServicesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jonathascabeco.stswebservicesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsWebServicesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsWebServicesProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonathascabeco/stswebservicesproject/StsWebServicesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathascabeco.stswebservicesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StsWebServicesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
